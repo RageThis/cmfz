68424a6812f773220b201cf73f3eb5e945875983
hl <501250891@qq.com>
2018-11-27 20:48:18 +0800

    login+selectAll(补充)

--- src/main/java/com/baizhi/cmfz/controller/AdminController.java
+++ src/main/java/com/baizhi/cmfz/controller/AdminController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -14,9 +16,17 @@ public class AdminController {
     @Autowired
     private AdminService as;
 
+
     @RequestMapping("/login")
-    public Map login(Admin a) {
-        System.out.println(a);
-        return as.selectOne(a);
+    public Map login(Admin a, String enCode, HttpSession session) {
+        String sessionkaptcha = (String) session.getAttribute("code");
+        Map map = new HashMap();
+        if (enCode.equals(sessionkaptcha)) {
+            return as.selectOne(a);
+        } else {
+            map.put("login", false);
+            return map;
+        }
+
     }
 }
